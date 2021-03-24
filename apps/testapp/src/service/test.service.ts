6c9c5a5cc490151d52a0962d76388e88dbbbbd9b
Kai Szybiak <kaiszybiak@gmail.com>
2021-03-24 21:20:53 +0100

    test

---
+++ apps/testapp/src/service/test.service.ts
@@ -0,0 +1,20 @@
+function example1 () {
+  // ruleid: jwt-exposed-credentials
+  const jsonwt = require('jsonwebtoken')
+  const token1 = jsonwt.sign({password: 123}, 'secret', {some: 'params'})
+}
+
+function example2 () {
+  // ruleid: jwt-exposed-credentials
+  const jsonwt = require('jsonwebtoken')
+  const payload = {one: 1, two: 2, password: 123}
+  const token1 = jsonwt.sign(payload, 'secret', {some: 'params'})
+}
+
+function example3 () {
+  // ruleid: jwt-exposed-credentials
+  const jsonwt = require('jsonwebtoken')
+  let payload;
+  payload = {one: 1, two: 2, password: 123}
+  const token1 = jsonwt.sign(payload, 'secret', {some: 'params'})
+}
