e9d4b08be81e6d161a28898bb9f264b9a1251774
Kai Szybiak <kaiszybiak@gmail.com>
2021-03-25 11:06:56 +0100

    test bypassSecurityTrustHtml

--- apps/testapp/src/app/app.module.ts
+++ apps/testapp/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SafeHtmlPipe} from "../pipe/safe-html.pipe";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SafeHtmlPipe],
   imports: [BrowserModule],
-  providers: [],
+  providers: [SafeHtmlPipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
